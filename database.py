407d6a2110f208043a014d176c33ff8714bcb1b2
Alan Borsato <37001712+aborsato@users.noreply.github.com>
2021-12-09 19:11:25 +0000

    make source files pretty ;)

--- database.py
+++ database.py
@@ -1,5 +1,14 @@
+# ░▄▀▀░▄▀▀▒▄▀▄
+# ░▀▄▄▒▄██░█▀█
+# Microsoft
+
+"""
+This module describes the SQLalchey table schema, to be used by the
+web app.
+"""
+
 from sqlalchemy import (
-    Column, 
+    Column,
     Integer,
     String,
     DateTime,
@@ -8,12 +17,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
+    """Represents User table."""
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
 
+
 class Metadata(Base):
+    """Represents File Metadata table."""
     __tablename__ = 'metadata'
     id = Column(Integer, primary_key=True)
     file_name = Column(String, nullable=False, unique=True)
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import datetime
-from dateutil.relativedelta import relativedelta
-import os
+# ░▄▀▀░▄▀▀▒▄▀▄
+# ░▀▄▄▒▄██░█▀█
+# Microsoft
 
-from flask import Flask, send_file, render_template, request, abort
-from sqlalchemy import create_engine, select, and_, or_
+"""
+This app serves static files from a specific location and checks
+if the given user (from Request Header or Cookie) is the owner
+or the file uploaded date is older than one year.
+"""
+
+import os
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+from flask import Flask, abort, request, send_file
+from sqlalchemy import and_, create_engine, or_, select
 from sqlalchemy.orm import Session
 from sqlalchemy.pool import SingletonThreadPool
 
-from database import Base, User, Metadata
+from database import Base, Metadata
 
 # TODO: change this variable to point to the root folder of your download path
 ROOT_DOWNLOAD_PATH = '.'
@@ -23,29 +33,28 @@ session = Session(engine, future=True)
 @app.route('/download/<file_name>')
 def download_file(file_name=None):
     # check user credentials (accepts request parameter or header)
-	user_name = request.args.get('user', request.headers.get('user', request.cookies.get('user', '')))
+    user_name = request.args.get('user', request.headers.get('user', request.cookies.get('user', '')))
 
-	# check if the user have access to the requested file and the file exists
-	# TODO: change the query to filter by your own criteria
-	one_year_back = datetime.datetime.today() + relativedelta(years=-1)
-	query_file = select(Metadata).where(
-		and_(
-			Metadata.file_name == file_name,
-			or_(
-				Metadata.owner == user_name, # the user is the owner of the file
-				Metadata.uploaded_date <= one_year_back # the file is older than 1 year
-			)
-		)
-	)
+    # check if the user have access to the requested file and the file exists
+    # TODO: change the query to filter by your own criteria
+    one_year_back = datetime.today() + relativedelta(years=-1)
+    query_file = select(Metadata).where(
+        and_(
+            Metadata.file_name == file_name,
+            or_(
+                Metadata.owner == user_name, # the user is the owner of the file
+                Metadata.uploaded_date <= one_year_back # the file is older than 1 year
+            )
+        )
+    )
 
-	# download the file if the record exists in the database
-	if session.execute(query_file).all():
-		file_path = os.path.join(ROOT_DOWNLOAD_PATH, file_name)
-		return send_file(file_path, as_attachment=True)
-	
-	# abort the request with 404 (NOT FOUND) otherwise
-	abort(404)
+    # download the file if the record exists in the database
+    if session.execute(query_file).all():
+        file_path = os.path.join(ROOT_DOWNLOAD_PATH, file_name)
+        return send_file(file_path, as_attachment=True)
+
+    # abort the request with 404 (NOT FOUND) otherwise
+    abort(404)
 
 if __name__ == "__main__":
     app.run()
-
